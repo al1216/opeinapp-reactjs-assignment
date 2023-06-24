94ffd0d4567c64ecf3dfd9be2d5ded26d1e50774
Aditya Kumar Gupta <adityakumargupta115@gmail.com>
2023-06-25 02:28:52 +0530

    pie graph

--- src/Pages/Dashboard/Activites.jsx
+++ src/Pages/Dashboard/Activites.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Activites.css";
 import axios from "axios";
-import { LineChart, Line, YAxis, CartesianGrid } from "recharts";
+import { LineChart, Line, XAxis,YAxis, CartesianGrid } from "recharts";
 
 export default function Activites() {
   let [prod, setProd] = useState([]);
@@ -37,8 +37,8 @@ export default function Activites() {
       </div>
       <div className="line-chart">
             <LineChart
-              width={1100}
-              height={250}
+              width={1050}
+              height={270}
               data={prod}
               margin={{
                 top: 15,
@@ -48,12 +48,12 @@ export default function Activites() {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
+              <XAxis />
               <YAxis />
               <Line
                 type="monotone"
                 dataKey="price"
                 stroke="#E9A0A0"
-                // activeDot={{ r: 8 }}
               />
               <Line type="monotone" dataKey="rating.count" stroke="#9BDD7C" />
             </LineChart>
--- src/Pages/Dashboard/Product.jsx
+++ src/Pages/Dashboard/Product.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Product.css";
+import axios from "axios";
+import { PieChart, Pie, Cell } from "recharts";
 
 export default function Product() {
+  let [prod1, setProd1] = useState([]);
+  let [prod2, setProd2] = useState([]);
+  let [prod3, setProd3] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/category/jewelery")
+      .then((res) => {
+        setProd1(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("https://fakestoreapi.com/products/category/women's%20clothing")
+      .then((res) => {
+        setProd2(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("https://fakestoreapi.com/products/category/men's%20clothing")
+      .then((res) => {
+        setProd3(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const data = [
+    { name: "Group A", value: prod2.length },
+    { name: "Group B", value: prod1.length },
+    { name: "Group C", value: prod3.length - 1 },
+  ];
+  const COLORS = ["#98D89E", "#EE8484", "#F6DC7D"];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }: any) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return <></>;
+  };
   return (
     <div className="product-right">
       <div className="heading-filter">
@@ -15,24 +68,48 @@ export default function Product() {
       </div>
       <div className="pie-legends">
         <div className="pie-graph">
-            <img src="pie.png" alt="" className="pie" />
+          {/* <img src="pie.png" alt="" className="pie" /> */}
+          <PieChart
+            width={180}
+            height={140}
+            margin={{ top: -130, right: 5, bottom: 5, left: -120 }}
+            
+          >
+            <Pie
+              data={data}
+              cx={200}
+              cy={200}
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={62}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
         </div>
         <div className="pie-legends-caption-icon">
-            <div className="legend1">
-                <img src="green.png" alt="" className="green-circle" />
-                <p className="caption-legend">Basic Tees</p>
-            </div>
-            <p className="legend-value">55%</p>
-            <div className="legend2">
-                <img src="yellow.png" alt="" className="yellow-circle" />
-                <p className="caption-legend">Custom Short Pants</p>
-            </div>
-            <p className="legend-value">31%</p>
-            <div className="legend3">
-                <img src="pink.png" alt="" className="pink-circle" />
-                <p className="caption-legend">Super Hoodies</p>
-            </div>
-            <p className="legend-value">14%</p>
+          <div className="legend1">
+            <img src="green.png" alt="" className="green-circle" />
+            <p className="caption-legend">Basic Tees</p>
+          </div>
+          <p className="legend-value">55%</p>
+          <div className="legend2">
+            <img src="yellow.png" alt="" className="yellow-circle" />
+            <p className="caption-legend">Custom Short Pants</p>
+          </div>
+          <p className="legend-value">31%</p>
+          <div className="legend3">
+            <img src="pink.png" alt="" className="pink-circle" />
+            <p className="caption-legend">Super Hoodies</p>
+          </div>
+          <p className="legend-value">14%</p>
         </div>
       </div>
     </div>

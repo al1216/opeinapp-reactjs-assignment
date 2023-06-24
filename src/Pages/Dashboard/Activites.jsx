38950f6c64eb0b256e2f77f7966c35b7812c1e41
Aditya Kumar Gupta <adityakumargupta115@gmail.com>
2023-06-25 01:59:04 +0530

    line chart at activities

--- src/Pages/Dashboard/Activites.jsx
+++ src/Pages/Dashboard/Activites.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Activites.css";
+import axios from "axios";
+import { LineChart, Line, YAxis, CartesianGrid } from "recharts";
 
 export default function Activites() {
+  let [prod, setProd] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => {
+        setProd(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="activites-right">
       <h1 className="activities-heading">Activities</h1>
@@ -21,7 +35,29 @@ export default function Activites() {
           </div>
         </div>
       </div>
-      
+      <div className="line-chart">
+            <LineChart
+              width={1100}
+              height={250}
+              data={prod}
+              margin={{
+                top: 15,
+                right: 20,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <YAxis />
+              <Line
+                type="monotone"
+                dataKey="price"
+                stroke="#E9A0A0"
+                // activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="rating.count" stroke="#9BDD7C" />
+            </LineChart>
+          </div>
     </div>
   );
 }
